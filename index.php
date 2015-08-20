476ae189e3d90db2579dffc7e437035da589e4e1
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-20 14:18:07 +0800

    create functions.php and index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('config.php');
+require_once('functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>投票システム</title>
+	</head>
+	<body>
+		<h1>お料理コンテスト</h1>
+	</body>
+</html>
